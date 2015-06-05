e1c504789a2ff8305033a8a9fc2914f28c2514f4
ushilb00 <bryce.hill@corporate.ingrammicro.com>
2015-06-05 09:53:38 -0700

    Adding user accounts framework

--- server.js
+++ server.js
@@ -24,9 +24,56 @@ var router = express.Router();
 var path = require('path');
 var express = require('express');
 var compression = require(compression);
+var secrets = require('./config/secrets');
+var mongoose = require('mongoose');
+var passport = rquire('passport');
+var session = equire('express-session');
+
+// Connect to the twitatron MongoDB
+mongoose.connect(secrets.db);
 
 // Load controllers
 var homeController = require('./controllers/home');
+var authController = require('./controllers/auth);
+
+// Create out Express application
+var app = express();
+
+// Tell Express to use sessions
+app.use(Session({
+	secret: secrets.sessionSecret,
+	resave: false,
+	saveUninitialized: false,
+}));
+
+// Use the passport package in our application
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add content compression middleware
+app.use(compression());
+
+// Add statis middleware
+var oneDay = 86400000;
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay }));
+
+// Add jade view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view ingine', 'jade');
+
+// create our express router
+var router.get('/', homeController.index);
+
+// landing page route
+router.get('/', homeController.index);
+
+// Auth routes
+router.get('/auth/twitter', authController.twitter);
+router.get('/auth/twitter/callback', authController.twitterCallback, function(req, res) {
+res.redirect(req.session.returnto || '/');});
+
+// Register all our routes
+app.use(router)
 
 // Landing page route
 router.get('/', homeController.index);
